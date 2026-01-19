591005cf9e2f883e660153cb03b608e24ce55c3b
TheAnh1101 <hatheanh8a@gmail.com>
2026-01-19 23:50:32 +0700

    commit

---
+++ src/test/java/com/example/condocare_swp/CondoCareSwpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.condocare_swp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondoCareSwpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
