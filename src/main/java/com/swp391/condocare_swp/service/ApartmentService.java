dc6543d634ee7fe8167e3e990bea645bc3ee0963
TheAnh01FPT <anhhthe171270@fpt.edu.vn>
2026-02-05 08:06:14 +0700

    apartmentlist

---
+++ src/main/java/com/swp391/condocare_swp/service/ApartmentService.java
@@ -0,0 +1,24 @@
+package com.swp391.condocare_swp.service;
+
+import com.swp391.condocare_swp.entity.Apartment;
+import com.swp391.condocare_swp.repository.ApartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ApartmentService
+{
+    private final ApartmentRepository apartmentRepository;
+
+    public Page<Apartment> getApartmentsByBuilding(String buildingId, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("number").ascending());
+        return apartmentRepository.findByBuildingId(buildingId, pageable);
+    }
+
+
+}
